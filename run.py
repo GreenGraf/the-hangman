7bd14f7fc0b1627bafdb1b35dd59884e8ecd2ac4
GreenGraf <moleary27@gmail.com>
2022-04-27 16:35:56 +0000

    test random word generator

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+word_list = ["python", "possum", "cat", "Container"]
+
+import random
+
+chosen_word = random.choice(word_list)
+
+print(chosen_word)
