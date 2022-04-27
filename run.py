e506892642dcf1ace8e1053f95881ac137f13cd4
GreenGraf <moleary27@gmail.com>
2022-04-27 16:47:46 +0000

    Check for letter in random word

--- run.py
+++ run.py
@@ -4,4 +4,10 @@ import random
 
 chosen_word = random.choice(word_list)
 
-print(chosen_word)
+guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Correct")
+    else:
+        print("Wrong")
