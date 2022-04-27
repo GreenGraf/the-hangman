d14583b4bc48a6f03b9e2eb067dddbc8faf98ee4
GreenGraf <moleary27@gmail.com>
2022-04-27 17:23:34 +0000

    Replace blank with correct chosen letter

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ chosen_word = random.choice(word_list)
 
 print(chosen_word)
 
+word_length = len(chosen_word)
+
 display = []
 for letter in chosen_word:
     display += "_"
@@ -11,11 +13,12 @@ print(display)
 
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Correct")
-    else:
-        print("Wrong")
+        display[position] = letter
+
+print(display)
 
 
 
