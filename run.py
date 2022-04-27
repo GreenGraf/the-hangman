fbb9cee716a67b94b6fef14dfa233d00ab980f08
GreenGraf <moleary27@gmail.com>
2022-04-27 19:08:39 +0000

    Adddisplay correct guess letter and add ASCII

--- run.py
+++ run.py
@@ -1,31 +1,104 @@
-word_list = ["python", "possum", "cat", "container"]
 import random
-chosen_word = random.choice(word_list)
 
-print(chosen_word)
-
-word_length = len(chosen_word)
-
-display = []
-for letter in chosen_word:
-    display += "_"
-print(display)
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 end_of_game = False
+word_list = ["python", "cat", "container", "leopard"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+lives = 6
+
+#Testing code
+print(chosen_word)
+
+#Create blank spaces for letter in random word
+display = []
+for _ in range(word_length):
+    display += "_"
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
+    """
+    Check guessed letter. Prints correct letter in place and if lives = 0 
+    then print "You Lose" and end game.
+    """
     for position in range(word_length):
         letter = chosen_word[position]
+       # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
-    print(display)
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
 
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has guessed all letters.
     if "_" not in display:
         end_of_game = True
-        print("WIN")
+        print("You win.")
 
-
+    #Print ASCII Hangman 
+    print(stages[lives])
 
