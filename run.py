8f5b8d102aa836389425227b24d270e3b28e080c
GreenGraf <moleary27@gmail.com>
2022-04-27 20:17:56 +0000

    Add clear console function

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import random
 import man_art
 import words
+import os
+
+#clears console after each guess
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 end_of_game = False
 chosen_word = random.choice(words.word_list)
@@ -20,6 +24,8 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    
+    clearConsole()
 
     """
     Check guessed letter. Prints correct letter in place and if lives = 0 
@@ -27,15 +33,16 @@ while not end_of_game:
     """
     if guess in display:
         print(f"You've already guessed {letter}")
-        
+
     for position in range(word_length):
         letter = chosen_word[position]
        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
-        elif guess != 
+      
 
     if guess not in chosen_word:
+        print(f"You guessed {guess}. Thant's not in the word. You lose a life. HA HA HA!")
         lives -= 1
         if lives == 0:
             end_of_game = True
