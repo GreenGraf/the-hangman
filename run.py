ff07829618967d2069d428ca2820162e6a2e6e79
GreenGraf <moleary27@gmail.com>
2022-04-27 17:14:40 +0000

    Add undersocre display for chosen word

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 word_list = ["python", "possum", "cat", "Container"]
-
 import random
-
 chosen_word = random.choice(word_list)
+
+print(chosen_word)
+
+display = []
+for letter in chosen_word:
+    display += "_"
+print(display)
 
 guess = input("Guess a letter: ").lower()
 
@@ -11,3 +16,6 @@ for letter in chosen_word:
         print("Correct")
     else:
         print("Wrong")
+
+
+
