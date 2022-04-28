0a324d17214966ddd2108c885ac8ba0b132ef200
GreenGraf <moleary27@gmail.com>
2022-04-28 20:06:59 +0000

    Clean up code and replace lamda with def

--- run.py
+++ run.py
@@ -2,35 +2,39 @@ import random
 import man_art
 import words
 import os
+from man_art import logo, stages, gallows
 
 
-#clears console after each guess
-clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+# clears console
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
 
-from man_art import logo, stages, gallows 
-print(logo)
 
 def game_rules():
+
     """
     Function for the rules section of the game
     """
+
     print('====================================')
     print()
     print('                 RULES:')
-    print()  
+    print()
     print('====================================')
-    print('  A secret word will be chosen at random.')   
+    print('  A secret word will be chosen at random.')
     print('  You must try to guess what the word is, one letter at a time.')
     print('  Guess a letter and press enter/return key.')
     print("  You have six chances to survive the hangman's noose.")
     print()
-    print("Would you like to try to beat the Hangman's noose?") 
+    print("Would you like to try to beat the Hangman's noose?")
     play_now = input("Enter yes or no: ")
     if play_now.lower() == "yes":
         clearConsole()
         play_game()
 
-    
     elif play_now.lower() == 'no':
         clearConsole()
         main()
@@ -43,33 +47,31 @@ def game_rules():
         print(' ***************')
         game_rules()
 
+
 def play_game():
     chosen_word = random.choice(words.word_list)
     word_length = len(chosen_word)
-    # print(gallows[stage])
-    #Testing code
     print(chosen_word)
-
     """
     Core logic of the game —
-    Check guessed letter. Prints correct letter in place and if lives = 0 
+    Check guessed letter. Prints correct letter in place and if lives = 0
     then print "You Lose" and end game.
     """
 
     end_of_game = False
     lives = 6
-    #Create blank spaces for letter in random word
+    # Create blank spaces for letter in random word
     display = []
     for _ in range(word_length):
         display += "_"
 
     while not end_of_game:
         guess = input("Guess a letter: ").lower()
-        
+
         clearConsole()
 
         if not guess.isalpha():
-            print("You did not enter a letter. Please try again by entering a letter.")
+            print("You did not enter a letter. Please try again.")
             continue
 
         if guess in display:
@@ -80,7 +82,7 @@ def play_game():
             if letter == guess:
                 display[position] = letter
                 print(f"You guessed '{guess}' ... good guess.")
-        
+
         if guess not in chosen_word:
             print(f"You guessed '{guess}'. Thant's not in the word. You lose a life. HA HA HA!")
             lives -= 1
@@ -89,18 +91,20 @@ def play_game():
                 print("You lose.")
                 play_again()
 
-        #Join all the elements in the list and turn it into a String.
+        # Join all the elements in the list and turn it into a String.
         print(f"{' '.join(display)}")
 
-        #Check if user has guessed all letters.
+        # Check if user has guessed all letters.
         if "_" not in display:
             end_of_game = True
             print(lives)
-            print("You win.")
+            print()
+            print("You win!")
             play_again()
 
-        #Print ASCII Hangman 
+        # Print ASCII Hangman
         print(stages[lives])
+
 
 def play_again():
     """
@@ -115,14 +119,13 @@ def play_again():
         clearConsole()
         play_game()
 
-    
     elif play_again.lower() == 'no':
         clearConsole()
 
         print()
         print()
         print("  COWARD! HA HA HA!")
-        print() 
+        print()
         print('  Come back soon ...')
         print()
         print('  ***************')
@@ -136,10 +139,12 @@ def play_again():
         print("  Please enter 'yes' or 'no'.")
         print('  ***************')
 
+
 def main():
     """
     Print welcome screen and call functions
     """
+    print(logo)
     print()
     print('====================================')
     print()
@@ -154,7 +159,7 @@ def main():
     if choice.lower() == "rules":
         clearConsole()
         game_rules()
-    
+
     elif choice.lower() == "play":
         clearConsole()
         play_game()
@@ -167,5 +172,6 @@ def main():
         print("  Please enter 'rules' or 'play'")
         print('  ***************')
         main()
-        
+
+
 main()
